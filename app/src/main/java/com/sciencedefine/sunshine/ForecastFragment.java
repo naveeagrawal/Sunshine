bf96c242f25a789fb1adf2a13acab249740d3307
Naveen Agrawal <navee.agrawal@gmail.com>
2015-08-11 16:37:44 +0530

    add onItemClickLIstener to the adapter

--- app/src/main/java/com/sciencedefine/sunshine/ForecastFragment.java
+++ app/src/main/java/com/sciencedefine/sunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.sciencedefine.sunshine;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -76,6 +79,14 @@ public class ForecastFragment extends Fragment {
         forecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
         ListView sListView = (ListView) rootView.findViewById(R.id.listview_forecast);
         sListView.setAdapter(forecastAdapter);
+        sListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getActivity(), forecastAdapter.getItem(i), Toast.LENGTH_LONG).show();
+                Intent detailIntent = new Intent(getActivity(), DetailActivity.class);
+                startActivity(detailIntent);
+            }
+        });
         return rootView;
     }
 
