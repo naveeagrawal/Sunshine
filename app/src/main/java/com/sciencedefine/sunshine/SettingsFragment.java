4b3c6814cada67b0429fe3d495143c4a1a9eeecd
Naveen Agrawal <navee.agrawal@gmail.com>
2015-08-14 12:20:18 +0530

    Add summary to preferences

--- app/src/main/java/com/sciencedefine/sunshine/SettingsFragment.java
+++ app/src/main/java/com/sciencedefine/sunshine/SettingsFragment.java
@@ -1,17 +1,60 @@
 package com.sciencedefine.sunshine;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceGroup;
 
 /**
  * Created by naveenagrawal on 12-Aug-15.
  */
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.pref_general);
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); ++i) {
+            Preference preference = getPreferenceScreen().getPreference(i);
+            if (preference instanceof PreferenceGroup) {
+                PreferenceGroup preferenceGroup = (PreferenceGroup) preference;
+                for (int j = 0; j < preferenceGroup.getPreferenceCount(); ++j) {
+                    updatePreference(preferenceGroup.getPreference(j));
+                }
+            } else {
+                updatePreference(preference);
+            }
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        updatePreference(findPreference(key));
+    }
+
+    private void updatePreference(Preference preference) {
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            listPreference.setSummary(listPreference.getEntry());
+        }
+        else if (preference instanceof EditTextPreference) {
+            EditTextPreference editTextPref = (EditTextPreference) preference;
+            preference.setSummary(editTextPref.getText()); }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
 }
